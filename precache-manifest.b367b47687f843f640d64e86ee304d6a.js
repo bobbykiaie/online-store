8d2e47e209d7f0dc351bc7a70857e64f23c00f1f
Bobby Kiaie <babakkiaie@Babaks-MacBook-Pro.local>
2020-10-01 13:27:20 -0700

    Updates

---
+++ precache-manifest.b367b47687f843f640d64e86ee304d6a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b25cf4c0a49d7270a45398b13d3735a",
+    "url": "/online-store/index.html"
+  },
+  {
+    "revision": "eb7cd3672adf979a1c23",
+    "url": "/online-store/static/css/main.4cb822c3.chunk.css"
+  },
+  {
+    "revision": "a081d2de01654aeaccca",
+    "url": "/online-store/static/js/2.7e52136d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/online-store/static/js/2.7e52136d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb7cd3672adf979a1c23",
+    "url": "/online-store/static/js/main.b6b886b0.chunk.js"
+  },
+  {
+    "revision": "1fd74144cdc6100ff345",
+    "url": "/online-store/static/js/runtime-main.2d1cc5f9.js"
+  }
+]);
