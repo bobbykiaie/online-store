c2330dd91d3e5279024f7d97ac92111d7c7d2b3e
Bobby Kiaie <babakkiaie@Babaks-MacBook-Pro.local>
2020-10-05 20:33:08 -0700

    Added signin/signup page

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
-import React from "react";
-import "./App.css";
-import HomePage from "./pages/homepage/homepage.component";
-import { Route, Link, Switch } from "react-router-dom";
-import ShopPage from './pages/shop/shop.components';
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import './App.css';
+
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component.jsx';
+import Header from './components/header/header.component.jsx';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
 
 
 function App() {
   return (
-    <div className="App">
+    <div>
+      <Header />
       <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route path="/shop" component={ShopPage} />
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
